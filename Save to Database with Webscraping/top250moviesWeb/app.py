ab00dde6942bbcbdb82f12f84ede94538125f91a
mahen2-cmd <mahenmughal2@gmail.com>
2023-08-12 20:55:20 +0500

    Saved stuff in a database. Now trying to show it on a website.

---
+++ Save to Database with Webscraping/top250moviesWeb/app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+import sqlite3
+
+app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello():
+#     return 'Hello, world!'
+
+
+@app.route('/')
+def index():
+    # Connect to the database
+    conn = sqlite3.connect('Top250Movies.db')
+    cursor = conn.cursor()
+
+    # Retrieve the data from the database
+    cursor.execute('SELECT * FROM top250movies')
+    rows = cursor.fetchall()
+
+    # Close the connection
+    conn.close()
+
+    # Render the template with the data
+    return render_template('index.html', rows=rows)
+
+
+
+
+if __name__ == '__main__':
+    app.run()
